c5885927733ffbb144a4add5c82502ae7a3865d0
yangkuo <yangkuo@rofine.com>
2023-04-17 09:32:23 +0800

    feat: helloworld 代码

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import * as THREE from 'three'
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
 )
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -18,10 +18,19 @@ scene.add(cube)
 
 camera.position.z = 5
 
-function animate() {
-  requestAnimationFrame(animate)
-  cube.rotation.x += 0.01
-  cube.rotation.y += 0.01
-  renderer.render(scene, camera)
+const animate = () => {
+    requestAnimationFrame(animate)
+    cube.rotation.x += 0.01
+    cube.rotation.y += 0.01
+    renderer.render(scene, camera)
 }
 animate()
+
+window.addEventListener('resize', () => {
+    // 重新设置相机宽高比例
+    camera.aspect = window.innerWidth / window.innerHeight
+    // 更新相机投影矩阵
+    camera.updateProjectionMatrix()
+    // 重新设置渲染器渲染范围
+    renderer.setSize(window.innerWidth, window.innerHeight)
+})
